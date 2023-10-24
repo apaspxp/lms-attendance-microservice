4b4dea7b879ce3c0e5c493c21ecf3a7fb2f633e6
rajesh1420621 <panigrahirajesh578@gmail.com>
2023-10-24 22:57:34 +0530

    Deploy code in jenkins file

--- src/main/java/com/lms/attendanceservice/LmsAttendanceServiceApplication.java
+++ src/main/java/com/lms/attendanceservice/LmsAttendanceServiceApplication.java
@@ -2,10 +2,10 @@ package com.lms.attendanceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class LmsAttendanceServiceApplication {
 
 	public static void main(String[] args) {
